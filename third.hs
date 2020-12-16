4290527f5b8d865f80463a82009914f3a7fae8f7
Tomas Brukner <tomas.brukner@gmail.com>
2020-12-16 18:57:00 +0100

    Curried version of third.

--- third.hs
+++ third.hs
@@ -13,7 +13,8 @@ traverseWithOffset _ _ _ [] = []
 main = do
     content <- getContents
     --putStrLn $ show $ product 
-    putStrLn $ show $ product $ map (\c -> length $ filter (== True) $ traverseMap isTree c $ lines content) config
+    putStrLn $ show $ product $ map (length . filter (== True) . uncurry (traverseMap isTree)) $ zip config (repeat $ lines content)
+    --putStrLn $ show $ product $ map (map (length . filter (== True) . traverseMap isTree) config) (repeat $ lines content)
     where config = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     --map (map (\c l -> length $ filter (== True) $ traverseMap isTree c l) config) (lines content)
     
